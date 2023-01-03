47ef7523586f24e5de5cf49bf00adcd11de1ff91
Ali Samur <97285821+SamurAy-01@users.noreply.github.com>
2023-01-03 14:30:34 +0300

    script.js
    js dosyası

---
+++ script.js
@@ -0,0 +1,110 @@
+var models = [
+    {
+        name : 'Bugatti Chiron',
+        image : 'img/bugatti.jpg',
+        link : 'https://www.youtube.com/'
+    },
+    {
+        name : 'Mazda',
+        image : 'img/mazda.jpgf',
+        link : 'https://www.youtube.com/'
+    },
+    {
+        name : 'Nissan GTR',
+        image : 'img/gtr.jpg',
+        link : 'https://www.youtube.com/'
+    },
+    {
+        name : 'Rolls Royce',
+        image : 'img/rolls-royce.jpg',
+        link : 'https://www.youtube.com/'
+    },
+    {
+        name : 'Supra',
+        image : 'img/supra.jpg',
+        link : 'https://www.youtube.com/'
+    }
+];
+
+var index = 0;
+var slaytCount = models.length;
+var interval;
+
+var settings={
+    duration : '1500',
+    random : true
+};
+
+init(settings);
+
+document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
+    index--;
+    showSlide(index);
+    console.log(index);
+});
+
+document.querySelector('.fa-arrow-circle-right').addEventListener('click',function(){
+    index++;
+    showSlide(index);
+    console.log(index);    
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval);
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings);
+    })
+})
+
+
+function init(settings){
+
+    var prev;
+    interval=setInterval(function(){
+        
+        if(settings.random){
+            // random index
+            do{
+                index = Math.floor(Math.random() * slaytCount);
+            }while(index == prev)
+            prev = index;
+        }else{
+            // artan index
+            if(slaytCount == index+1){
+                index = -1;
+            }
+            showSlide(index);
+            console.log(index);
+            index++;
+        }
+        showSlide(index);
+
+    },settings.duration)
+  
+
+}
+
+
+
+function showSlide(i){
+
+    index = i;
+
+    if (i<0) {
+        index = slaytCount - 1;
+    }
+    if(i >= slaytCount){
+        index =0;
+    }
+
+    document.querySelector('.card-title').textContent = models[index].name;
+
+    document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+    
+    document.querySelector('.card-link').setAttribute('href',models[index].link);
+}
